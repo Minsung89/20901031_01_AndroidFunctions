ea01ce89085963bca4c3bd2ef26522a425e5eece
Minsung89 <minsung0217@gmail.com>
2019-10-31 20:44:12 +0900

    로그인 액티비티 추가  - 각 색상 변경

---
+++ app/src/main/java/com/tjoeum/a20901031_01_androidfunctions/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.tjoeum.a20901031_01_androidfunctions
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
