fafac85e77391c510d16dcb1ebbb6cdd28e80924
Minsung89 <minsung0217@gmail.com>
2019-10-31 22:08:56 +0900

    첨부된 데이터 받아서 활용하기 (MainActivity)

--- app/src/main/java/com/tjoeum/a20901031_01_androidfunctions/MainActivity.kt
+++ app/src/main/java/com/tjoeum/a20901031_01_androidfunctions/MainActivity.kt
@@ -2,11 +2,19 @@ package com.tjoeum.a20901031_01_androidfunctions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //intent
+        var receivedName = intent.getStringExtra("name")
+        var receivedNumber = intent.getStringExtra("userNumber")
+
+        userNameTxt.text = receivedName
+        userNumberTxt.text = receivedNumber
     }
 }
