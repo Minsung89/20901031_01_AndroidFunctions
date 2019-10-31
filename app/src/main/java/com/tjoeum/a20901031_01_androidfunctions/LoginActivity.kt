e94a7054b88503cae71c290fe76dd4136757aca9
Minsung89 <minsung0217@gmail.com>
2019-10-31 21:14:49 +0900

    연습문제 풀이  - 아이디 부여  - 이벤트 처리  - 텍스트 값 받아오기  - 토스트 출력

--- app/src/main/java/com/tjoeum/a20901031_01_androidfunctions/LoginActivity.kt
+++ app/src/main/java/com/tjoeum/a20901031_01_androidfunctions/LoginActivity.kt
@@ -2,11 +2,25 @@ package com.tjoeum.a20901031_01_androidfunctions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    //이름과 회원번호를 입력하고 로그인 버튼을 누르면
+    //Ex 10번 구민성 회원님 환영합니다.라는 Toast를 띄우자
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+
+        loginBtn.setOnClickListener {
+            var loginName = loginName.text.toString()
+            var userNumber = uesrNumber.text.toString()
+            Toast.makeText(this,"이름 : ${loginName} 회원번호 : ${userNumber}",Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
